a42689180dd83ed5fdd11defdd7e82892b95751f
Marvin Luchs <marvin.luchs@holisticon.de>
2019-12-17 11:33:34 +0100

    link to details

--- libs/overview/src/lib/overview/overview.component.ts
+++ libs/overview/src/lib/overview/overview.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RouterAppState } from '@si-bre/routing';
+import { Search } from '@si-bre/search';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'si-bre-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent {
 
-  constructor() { }
+  queryParams$: Observable<Search>;
 
-  ngOnInit() {
+  constructor(
+    private store: Store<RouterAppState>
+  ) {
+    // 💣 don't do this in production
+    this.queryParams$ = this.store.select(state => state.router.state.root.queryParams).pipe(
+      map(queryParams => ({
+        ...queryParams,
+        pnr: '123'
+      } as Search))
+    );
   }
 
 }
